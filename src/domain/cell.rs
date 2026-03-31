0352ec88e71c2058325c6498c71d9c9260ba58fa
Beagle-Pete <44248961+Beagle-Pete@users.noreply.github.com>
2026-03-31 11:06:19 -0400

    Feature - Add csv reader (#2)
    - Add dependency `csv`
    - Add new `CsvReader` to read data from a .csv file
        - Implements trait `DBReader`
        - All data is outputted as a String type
    - Derive `PartialEq` for `PrimTypeData` and `APIError`
    - Implement `Display` on `PrimTypeData`
        - Prints out the data field as a string

--- src/domain/cell.rs
+++ src/domain/cell.rs
@@ -5,3 +5,12 @@ pub struct Cell {
     pub data: Result<PrimTypeData, APIError>,
     pub cell_address: (usize, usize),
 }
+
+impl Cell {
+    pub fn new(data: Result<PrimTypeData, APIError>, cell_address: (usize, usize)) -> Self {
+        Self {
+            data,
+            cell_address,
+        }
+    }
+}
